218109b9571dc828aa099f463091e0e352446e60
Mohamed Dewidar <Mohamed Dewidar>
2016-11-26 19:12:14 +0000

    Added some basic code for data manipulation later

---
+++ EventR/EventR/EventData/ScheduledEvent.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EventR
+{
+    class ScheduledEvent
+    {
+        public string name;
+        public List<EventDate> dates;
+    }
+}
